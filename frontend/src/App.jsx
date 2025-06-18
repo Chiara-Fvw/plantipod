4e1d21869ab51cbac5abefdcf0f0ea331d19a952
chiara <chiara.fvw@gmail.com>
2025-06-18 10:14:07 +0200

    Start the landing page, configure the index.css with tailwind and modify accordingly the main.jsx file

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,35 +1,28 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
-function App() {
-  const [count, setCount] = useState(0)
-
+const App = () => {
+  const handleClick = (section) => {
+    alert(`You clicked on ${section}`);
+  }
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
+    <main>
+      <h1 className="text-3xl font-bold text-green-600">🌿 PlantiPod</h1>
+      
+      <p>
+        PlantiPod is your digital garden: discover helpful blog posts, practical courses, and mindful podcasts to nurture your plants and your soul.
       </p>
-    </>
+
+      <section className='landing-card-container'>
+        <div className='landing-card' onClick={() => handleClick('Blog')} style={{ cursor: 'pointer' }}>
+          📝 Blog
+        </div>
+        <div className='landing-card' onClick={() => handleClick('Courses')} style={{ cursor: 'pointer' }}>
+          🎓 Courses
+        </div>
+        <div className='landing-card' onClick={() => handleClick('Podcast')} style={{ cursor: 'pointer' }}>
+          🎧 Podcast
+        </div>
+      </section>
+    </main>
   )
 }
 
-export default App
+export default App;
