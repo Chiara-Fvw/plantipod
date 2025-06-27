183a5bfc16c222121daaa8e87585d455e10e1278
chiara <chiara.fvw@gmail.com>
2025-06-27 19:02:55 +0200

    Add blog cards and style them.

--- frontend/src/components/Blog.jsx
+++ frontend/src/components/Blog.jsx
@@ -1,14 +1,16 @@
 import React, { useEffect, useState } from "react";
 
 const Post = ({data}) => {
-      console.log(data.image_url);
   return (
-    <div className="mb-4 break-inside-avoid rounded overflow-hidden shadow-lg"
-        style={{ backgroundImage: `url('http://localhost:3001/${data.image_url}')`, backgroundSize: 'cover', backgroundPosition: 'center' }}>
-      <div className="bg-white opacity-80 p-4 ">
-        <h1 className="text-lg font-semibold mb-2">{data.title}</h1>
-        <p>{new Date(data.created_at).toLocaleDateString()}</p>
-        <button className="mt-2 inline-block px-4 py-2 bg-main text-white rounded hover:bg-secondary"> Read → </button>
+    <div className="relative w-full max-w-md overflow-hidden shadow-lg mb-4 group transition-transform duration-300 hover:scale-[1.02]">
+      <img 
+        src={`http://localhost:3001/${data.image_url}`} 
+        alt="Post thumbnail" 
+        className="w-full object-cover"
+      />
+      <div className="absolute bottom-0 left-0 right-0 bg-white p-4 mx-2 my-1 group-hover:bg-main transition-colors duration-300">
+        <p className="text-gray-700 text-center font-medium group-hover:opacity-0 transition-opacity duration-300">{data.title}</p>
+        <p className="text-white text-center font-medium opacity-0 group-hover:opacity-100 transition-opacity duration-300 absolute inset-0 flex items-center justify-center">Read Post →</p>
       </div>
     </div>
   )
@@ -31,10 +33,10 @@ const BlogRight = () => {
   }, []);
 
   return (
-    <div className="md:w-1/2 w-full flex flex-col md:h-full">
+    <div className="md:w-1/2 w-full flex flex-col md:h-full mx-6">
               <div><p>Filtering goes here</p></div>
       <div className="min-h-[250px] md:min-h-0 md:flex-1 flex">
-        <div className="columns-2 gap-4">
+        <div className="md:columns-2 gap-4">
           {posts.map((postData) => (
             <Post key={postData.id} data={postData} />
           ))}
--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -7,7 +7,7 @@ const HomeRight = ({onClick}) => {
           onClick={() => onClick(item)}
           className="min-h-[250px] md:min-h-0 md:flex-1 flex items-center justify-center bg-cover bg-center relative cursor-pointer group transform transition-transform duration-300 ease-in-out"
           style={{ backgroundImage: `url('/img/${item.toLowerCase()}.png')` }}>
-          <div className="relative z-10 bg-white px-6 py-3 rounded-md group-hover:translate-y-2 transition-all duration-300">
+          <div className="relative z-10 bg-white px-6 py-3 group-hover:translate-y-2 transition-all duration-300">
             <h2 className="text-main text-4xl ">{item.toUpperCase()}</h2>
           </div>
         </div>
