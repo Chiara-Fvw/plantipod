f6fb45971a8f4cfdf7d7968552d80e84a8060ca9
chiara <chiara.fvw@gmail.com>
2025-10-23 14:03:31 +0200

    Add front end url for cors

--- backend/utils/config.js
+++ backend/utils/config.js
@@ -3,6 +3,8 @@ dotenv.config();
 
 const config = {
     PORT: process.env.PORT || 3001,
+    NODE_ENV: process.env.NODE_ENV || 'development',
+    FRONTEND_URL: process.env.FRONTEND_URL,
     DB_USER: process.env.DB_USER,
     DB_PASSWORD: process.env.DB_PASSWORD,
     DB_HOST: process.env.DB_HOST || 'localhost',
